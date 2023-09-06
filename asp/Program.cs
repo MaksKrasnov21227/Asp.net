ce23e1bf43e101a3d477cbd59bedfbd4480b10e7
MaksKrasnov21227 <max21227@yandex.ru>
2023-09-06 19:01:49 +0700

    new commit

--- asp/Program.cs
+++ asp/Program.cs
@@ -2,5 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
-
+app.MapGet("/test", (HttpContext context) => TestTask(context));
 app.Run();
+
+async Task TestTask(HttpContext httpContext)
+{
+    var msg = "Test for asp net core message";
+    await httpContext.Response.WriteAsync(msg);
+}
