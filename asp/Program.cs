eb45de4f87ba59af02dd0a2ae74ef2e336c49769
MaksKrasnov21227 <max21227@yandex.ru>
2023-09-06 21:08:16 +0700

    com

--- asp/Program.cs
+++ asp/Program.cs
@@ -1,12 +1,45 @@
+using asp;
+using Microsoft.AspNetCore.Server.Kestrel.Core.Features;
+using System.Text.RegularExpressions;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
+DbContext dbContext = new DbContext();
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/test", (HttpContext context) => TestTask(context));
+app.Run(TestTask);
 app.Run();
 
 async Task TestTask(HttpContext httpContext)
 {
-    var msg = "Test for asp net core message";
-    await httpContext.Response.WriteAsync(msg);
+   var request = httpContext.Request;
+   var response = httpContext.Response;
+   string path = request.Path;
+    string guidExpression = @"^/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$";
+    if (path == "/")
+    {
+        await response.WriteAsync("Welcome to the Top Academy");
+    }
+    else if(path =="/all" && request.Method == "GET")
+    {
+        await dbContext.GetAll(response);
+    }
+    else if(Regex.IsMatch(path, guidExpression) && request.Method =="GET")
+    {
+        Guid? id = Guid.Parse(path.Split('/')[1]);
+        await dbContext.Get(id, response);
+    }
+    else if(path=="/add" && request.Method == "POST")
+    {
+        await dbContext.Insert(request, response);
+    }
+    else if(path== "/update" && request.Method == "POST") {
+        await dbContext.Update(request, response);
+
+    }
+    else if (Regex.IsMatch(path, guidExpression) && request.Method == "DELETE" )
+    {
+        Guid? id = Guid.Parse(path.Split('/')[1]);
+        await dbContext.Delete(id, response);
+    }
+
 }
--- asp/User.cs
+++ asp/User.cs
@@ -1,6 +1,137 @@
-﻿namespace asp
+﻿using Microsoft.AspNetCore.Http;
+
+namespace asp
 {
-    public class Class
+    public class User
     {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Password { get; set; }
+
+        public User()
+        {
+            Id = Guid.NewGuid();
+            Name = string.Empty;
+            Password = string.Empty;
+        }
+
+        public User(Guid id, string? name, string? password)
+        {
+            Id = id;
+            Name = name;
+            Password = password;
+        }
+    }
+    public interface IDbModel<T>
+    {
+        public Task Get(Guid? id, HttpResponse response);
+        public Task GetAll(HttpResponse response);
+
+        public Task Delete(Guid? id, HttpResponse response);
+        public Task Update(HttpRequest request, HttpResponse response);
+        public Task Insert(HttpRequest request, HttpResponse response);
+    }
+
+    public class DbContext : IDbModel<User>
+    {
+        List<User> models = new()
+        {
+            new User ( Guid.NewGuid(),"Jhon","qwerty"),
+            new User ( Guid.NewGuid(),"Alise","123"),
+            new User ( Guid.NewGuid(),"Denis","456"),
+            new User ( Guid.NewGuid(),"Gleb","789"),
+            new User ( Guid.NewGuid(),"Ivan","qwe75572rty"),
+            new User ( Guid.NewGuid(),"Semen","7257dgh"),
+            new User ( Guid.NewGuid(),"Sergey","dfhdsh"),
+            new User ( Guid.NewGuid(),"Vasya","aefj")
+        };
+        public async Task Delete(Guid? id, HttpResponse response)
+        {
+            User? user = models.FirstOrDefault(x => x.Id == id);
+            if(user != null) {
+                models.Remove(user);
+
+            }
+            else
+            {
+                response.StatusCode = 400;
+                await response.WriteAsJsonAsync(new { Error = "Incorrect data" });
+            }
+
+        }
+
+        public async Task Get(Guid? id, HttpResponse response)
+        {
+            User? user=models.FirstOrDefault(x => x.Id == id);
+            if(user!=null)
+            {
+                await response.WriteAsJsonAsync(user);
+
+            }
+            else
+            {
+                response.StatusCode = 404;
+                await response.WriteAsJsonAsync(new {Error= "User not found"});
+            }
+        }
+
+        public async Task GetAll(HttpResponse response)
+        {
+            await response.WriteAsJsonAsync(models);
+        }
+
+        public async Task Insert(HttpRequest request, HttpResponse response)
+        {
+            try
+            {
+                var storage = await request.ReadFromJsonAsync<User>();
+                if (storage!=null)
+                {
+                    User user = new()
+                    {
+                        Id = storage.Id,
+                        Name = storage.Name,
+                        Password = storage.Password
+                    };
+                    models.Add(user);
+                    await response.WriteAsJsonAsync(user);
+                }
+                else
+                {
+                    throw new Exception("Incorrect data");
+                }
+            }
+            catch(Exception ex) { }
+            {
+                response.StatusCode = 400;
+                await response.WriteAsJsonAsync(new { Error = "Incorrect data" });
+            }
+        }
+
+        public async Task Update(HttpRequest request, HttpResponse response)
+        {
+           var user= await request.ReadFromJsonAsync<User>();
+            if (user!=null)
+            {
+               User? u= models.FirstOrDefault(x => x.Id == user.Id);
+                if (u != null)
+                {
+                    u.Name = user.Name;
+                    u.Password = user.Password;
+                }
+                //foreach (var item in models)
+                //{
+                //    if (user.Id == item.Id)
+                //    {
+                //        item.Name = user.Name;
+                //    }
+                //}
+            }
+            else
+            {
+                response.StatusCode = 400;
+                await response.WriteAsJsonAsync(new { Error = "Incorrect data" });
+            }
+        }
     }
 }
